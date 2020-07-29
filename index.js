8d8d86fa69a7d2484ce6764b7c3a9fbdba199d1f
chahiinaz <chahiinaz@gmail.com>
2020-07-29 21:47:48 +0200

    Create simple image route that adds entries when image post

--- index.js
+++ index.js
@@ -67,6 +67,21 @@ app.get("/profile/:id", (req, res) => {
   }
 });
 
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(404).json("not found");
+  }
+});
+
 app.listen(3000, () => {
   console.log("app is running on port 300");
 });
