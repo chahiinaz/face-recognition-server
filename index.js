fe491429aa66004b43a6743a786031d777716dad
chahiinaz <chahiinaz@gmail.com>
2020-07-29 21:24:34 +0200

    Hardcoded a 'database' in order to start routes

--- index.js
+++ index.js
@@ -2,6 +2,34 @@ const express = require("express");
 
 const app = express();
 
+const database={
+  users:[
+    {
+    id:"1",
+    name:"Piet"
+    email:"piet@piet.com"
+    password:"1234"
+    entries:0
+    joined: new Date()
+    },
+    {
+    id:"2",
+    name:"Jan"
+    email:"jan@jan.com"
+    password:"1234"
+    entries:0
+    joined: new Date()
+    }
+
+  ]
+}
+
+app.get("/", (req, res) => {
+  res.send("this is working");
+});
+app.post("/signin", (req, res) => {
+  res.send("singin");
+});
 app.listen(3000, () => {
   console.log("app is running on port 300");
 });
